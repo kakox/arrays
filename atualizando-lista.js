9203fae609ea6fb5f13e941566ca6e42f83c456d
kako <kauanribeirobravo@gmail.com>
2023-02-19 17:16:40 -0300

    att

---
+++ atualizando-lista.js
@@ -0,0 +1,12 @@
+const nomes = ['João', 'Ana', 'Caio', 'Lara', 'Marjorie', 'Léo'];
+
+
+
+// adicionar um nome na lista com:
+// nomes.push("Rodrigo");
+// ou adicionar ele direto no nomes.splice 
+// splice é um metodo que altera o array original
+
+nomes.splice(1, 2, "Rodrigo");
+
+console.log (nomes);
