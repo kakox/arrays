e07867fe42b9e33f1c86fc2b2adf2b055abfa84b
kako <kauanribeirobravo@gmail.com>
2023-02-21 04:54:37 -0300

    att

---
+++ for.js
@@ -0,0 +1,14 @@
+const numeros = [100, 200, 300, 400, 500, 600];
+
+for (let i = 0; i < numeros.length; i ++) {
+
+// Primeira expressão: é executada apenas uma única vez 
+// Segunda expressão: condição de execução
+// Terceira expressão: é executada sempre ao final do bloco
+
+console.log(numeros[i]);
+
+}
+
+
+
---
+++ map-nomes.js
@@ -0,0 +1,13 @@
+const nomes = ['ana Julia', 'Caio vinicius', 'BIA silva'];
+
+
+// const nomesPadronizados = nomes.map((nome) => {
+
+//     return nome.toUpperCase();
+
+// });
+
+
+const nomesPadronizados = nomes.map((nome) => nome.toUpperCase());
+
+console.log(nomesPadronizados);
